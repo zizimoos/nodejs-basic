bb22ef7d360c5abf6e5f2a2b740e1501baf6b97f
남형석 <azerc@MacMini.local>
2022-06-12 13:26:39 +0900

    add fetch for login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,6 +7,23 @@ function login() {
     id: id.value,
     password: password.value,
   };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        alert("로그인 성공");
+        window.location.href = "/";
+      } else {
+        alert("로그인 실패");
+      }
+    });
 }
 
 loginBtn.addEventListener("click", login);

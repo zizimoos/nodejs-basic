a3e637122d67cd39df901f259b23b860f4b4c28b
남형석 <azerc@MacMini.local>
2022-06-12 12:45:18 +0900

    making javascript in frondend

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+const __dirname = path.resolve();
 
 import homeRoutes from "./src/route/home.route.js";
 
@@ -7,8 +9,10 @@ export const app = express();
 
 app.use(cors("*"));
 app.use(express.json());
+
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", homeRoutes);
 app.use("/login", homeRoutes);

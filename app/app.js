6a6fdfa1dddaeafde2d7615b42c7bd70e69ac019
남형석 <azerc@MacMini.local>
2022-06-12 12:17:01 +0900

    restructuring again

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import homeRoutes from "./route/home.route.js";
+import homeRoutes from "./src/route/home.route.js";
 
 export const app = express();
 
---
+++ app/src/bin/www.js
@@ -0,0 +1,10 @@
+import { app } from "../src/app.js";
+import dotenv from "dotenv";
+import ip from "ip";
+
+dotenv.config();
+const PORT = process.env.SERVER_PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(` 🟢 Server is running on ${ip.address()} port ${PORT}`);
+});

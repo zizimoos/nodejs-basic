f02e9c5f9365e90233f94d203cd9a590f48de7c5
남형석 <azerc@MacMini.local>
2022-06-12 16:29:54 +0900

    styling login

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,7 +7,8 @@ class User {
 
   login() {
     const { id, password } = UserStorage.getUserInfo(this.body.id);
-
+    //  유저가 입력한 위의 id는 저장된 데이타에 없으면, undefine을 리턴하고, 에러를 발생시킨다.
+    //  그냥, 유저테이블에서 아이디를 검색해서 있어 없음을 판단하면 안되나?
     if (id === this.body.id) {
       if (password === this.body.password) {
         return { success: true, message: "로그인 성공" };
